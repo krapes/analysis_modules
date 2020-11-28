75364f7f43ef78e0e1ce10f38a9652e2e340e900
krapes <kerri.rapes@gmail.com>
2020-11-28 15:20:55 -0300

    Feature: remove credential_path

--- src/Flow/Flow.py
+++ src/Flow/Flow.py
@@ -13,9 +13,8 @@ from anycache import anycache
 
 from src import SankeyFlow
 
-credential_path = "/home/kerri/bigquery-jaya-consultant-cosmic-octane-88917-c46ba9b53a3b.json"
-assert os.path.exists(credential_path)
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
+# credential_path = "/home/kerri/bigquery-jaya-consultant-cosmic-octane-88917-c46ba9b53a3b.json"
+
 project_id = 'cosmic-octane-88917'
 client = bigquery.Client(project=project_id)
 
